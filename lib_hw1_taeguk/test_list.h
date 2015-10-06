e86013ec1d4163c3b21b1ef281694b633242ddc9
taeguk <xornrbboy@gmail.com>
2015-10-06 22:47:43 +0900

    wrapping list functions finished except list_push. (but code is not tested.)

--- lib_hw1_taeguk/test_list.h
+++ lib_hw1_taeguk/test_list.h
@@ -8,6 +8,25 @@ struct ListItem {
 	int data;
 };
 
-bool list_less_func(const struct list_elem *a, const struct list_elem *b, void *aux);
+bool l_less_func(const struct list_elem *a, const struct list_elem *b, void *aux);
+
+bool wrap_list_insert(struct Request *req, struct WrapDataStructure *wds[]);
+bool wrap_list_splice(struct Request *req, struct WrapDataStructure *wds[]);
+bool wrap_list_push(struct Request *req, struct WrapDataStructure *wds[]);
+bool wrap_list_push_front(struct Request *req, struct WrapDataStructure *wds[]);
+bool wrap_list_push_back(struct Request *req, struct WrapDataStructure *wds[]);
+bool wrap_list_remove(struct Request *req, struct WrapDataStructure *wds[]);
+bool wrap_list_pop_front(struct Request *req, struct WrapDataStructure *wds[]);
+bool wrap_list_pop_back(struct Request *req, struct WrapDataStructure *wds[]);
+bool wrap_list_front(struct Request *req, struct WrapDataStructure *wds[]);
+bool wrap_list_back(struct Request *req, struct WrapDataStructure *wds[]);
+bool wrap_list_size(struct Request *req, struct WrapDataStructure *wds[]);
+bool wrap_list_empty(struct Request *req, struct WrapDataStructure *wds[]);
+bool wrap_list_reverse(struct Request *req, struct WrapDataStructure *wds[]);
+bool wrap_list_sort(struct Request *req, struct WrapDataStructure *wds[]);
+bool wrap_list_insert_ordered(struct Request *req, struct WrapDataStructure *wds[]);
+bool wrap_list_unique(struct Request *req, struct WrapDataStructure *wds[]);
+bool wrap_list_max(struct Request *req, struct WrapDataStructure *wds[]);
+bool wrap_list_min(struct Request *req, struct WrapDataStructure *wds[]);
 
 #endif
