0fd7947d5de8b8c72343a48cf821009119c8bae8
taeguk <xornrbboy@gmail.com>
2015-11-11 01:20:14 +0900

    modify struct file

--- pintos/src/filesys/file.c
+++ pintos/src/filesys/file.c
@@ -9,6 +9,12 @@ struct file
     struct inode *inode;        /* File's inode. */
     off_t pos;                  /* Current position. */
     bool deny_write;            /* Has file_deny_write() been called? */
+    
+#ifdef USERPROG
+    /* added by taeguk */
+    int fd;                     /* File descriptor of file. */
+    struct list_elem file_elem;
+#endif
   };
 
 /* Opens a file for the given INODE, of which it takes ownership,
@@ -20,6 +26,7 @@ file_open (struct inode *inode)
   struct file *file = calloc (1, sizeof *file);
   if (inode != NULL && file != NULL)
     {
+      struct thread *t = thread_current ();
       file->inode = inode;
       file->pos = 0;
       file->deny_write = false;
