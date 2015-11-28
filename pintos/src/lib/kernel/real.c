fedb31e00ad4a6947d4342277ae5c0f7700713a6
taeguk <xornrbboy@gmail.com>
2015-11-28 18:18:24 +0900

    real arithmetic library coding finished. but not tested...

--- pintos/src/lib/kernel/real.c
+++ pintos/src/lib/kernel/real.c
@@ -7,3 +7,61 @@
 #define FP_Q_BITS     14
 
 #define FP_F    (1 << FP_Q_BITS)
+
+real real_from_int (int32_t n)
+{
+  return n * FP_F;
+}
+
+int32_t real_to_int (real x)
+{
+  return x / FP_F;
+}
+
+int32_t real_to_int_nearest (real x)
+{
+  return x >= 0 ?
+    (x + FP_F / 2) / FP_F :
+    (x - FP_F / 2) / FP_F ;
+}
+
+real real_add_rr (real x, real y)
+{
+  return x + y;
+}
+
+real real_add_ri (real x, int32_t n)
+{
+  return real_add_rr (x, real_from_int (n));
+}
+
+real real_sub_rr (real x, real y)
+{
+  return x - y;
+}
+
+real real_sub_ri (real x, int32_t n)
+{
+  return real_sub_rr (x, real_from_int (n));
+}
+
+real real_mul_rr (real x, real y)
+{
+  return (real) (((int64_t) x) * y / FP_F);
+}
+
+real real_mul_ri (real x, int32_t n)
+{
+  return x * n;
+}
+
+real real_div_rr (real x, real y)
+{
+  return (real) (((int64_t) x) * FP_F / y);
+}
+
+real real_div_ri (real x, int32_t n)
+{
+  return x / n;
+}
+
